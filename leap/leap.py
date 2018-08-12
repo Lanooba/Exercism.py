155250b471a3c9c00cd66e2d30e6d22a13f993be
Lanooba <lanooba@users.noreply.github.com>
2018-08-12 13:33:15 -0400

    3 tests passing; 1 failing: test for 2000 is leap year

--- leap/leap.py
+++ leap/leap.py
@@ -1,6 +1,11 @@
 def is_leap_year(year):
     remainder4 = year%4
     remainder100 = year%100
-    if remainder4 != 0: #and remainder100 !=0:
+    remainder400 = year%400
+    if remainder4 != 0: 
         return False
+    elif remainder100 != 0:
+        return True 
+    elif remainder400 != 0:
+        return False    
     
